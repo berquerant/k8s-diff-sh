a970b0a6f9e80dc744c63df98d84d6c90aba91ba
berquerant <sin@sin-pro.local>
2024-06-14 20:42:40 +0900

    Remove object.py

--- tests/script/object/test.sh
+++ tests/script/object/test.sh
@@ -9,16 +9,6 @@ right="tests/data/object/right.yaml"
 gotd="$(mktemp -d)"
 mkdir -p "$gotd"
 
-test_object_py_sh_diff() {
-    ./object.py.sh "$left" "$right" > "${gotd}/got"
-    diff -u "${thisd}/py.golden.diff" "${gotd}/got"
-}
-
-test_object_py_sh_diff_id() {
-    DIFF_ID=1 ./object.py.sh "$left" "$right" > "${gotd}/got"
-    diff -u "${thisd}/golden.id.diff" "${gotd}/got"
-}
-
 test_object_sh_diff() {
     ./object.sh "$left" "$right" > "${gotd}/got"
     diff -u "${thisd}/golden.diff" "${gotd}/got"
@@ -34,9 +24,3 @@ test_run_multi "test_object_py_sh_diff" \
                "test_object_py_sh_diff_id" \
                "test_object_sh_diff" \
                "test_object_sh_diff_id"
-ret=$?
-test_log "----------"
-test_log "NOTE: diff of diff goldens"
-diff -u "${thisd}/py.golden.diff" "${thisd}/golden.diff"
-test_log "----------"
-exit $ret
--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-pip install PyYAML==6.0.1
-
 thisd="$(cd $(dirname $0); pwd)"
 find "${thisd}/script" -type f -name test.sh | xargs -n 1 bash
